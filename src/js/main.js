1d2e7ccd92618f4312a04237cba07e059d05f62c
PlanTain <glebmarennyj@gmail.com>
2022-04-26 16:18:59 +0300

    ver2

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,5 @@
- 
+import '../scss/main.scss';
+import '../index.html';
+
+const a = 5;
+console.log(a);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 const isProd = !isDev;
@@ -14,7 +15,16 @@ module.exports = {
     entry: './js/main.js',
     output: {
         filename: `./js/${fileName(`js`)}`,
-        path: path.resolve(__dirname, 'app')
+        path: path.resolve(__dirname, 'app'),
+        publicPath: ''
+    },
+    devServer: {
+        historyApiFallback: true,
+        static: path.resolve(__dirname, 'app'),
+        open: true,
+        compress: true,
+        hot: true,
+        port: 5000
     },
     plugins: [
         new HTMLWebpackPlugin({
@@ -24,6 +34,85 @@ module.exports = {
                 collapseWhitespace: isProd
             }
         }),
-        new CleanWebpackPlugin()
-    ]
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: `./css/${fileName(`css`)}`
+        }),
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: path.resolve(__dirname, 'src/assets'), to: path.resolve(__dirname, 'app')}
+            ]
+        })
+    ],
+    devtool: isProd ? false : 'source-map',
+    module: {
+        rules: [
+            {
+                test: /\.html$/, 
+                use: {
+                    loader: "html-loader",
+                    options: {
+                        esModule: false
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: (resourcePath, context) => {
+                                return path.relative(path.dirname(resourcePath), context) + '/';
+                            },
+                        }
+                    },
+                    "css-loader",
+                ],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                        options: {
+                            publicPath: (resourcePath, context) => {
+                                return path.relative(path.dirname(resourcePath), context) + '/';
+                            },
+                        }
+                    },
+                    "css-loader",
+                    "sass-loader"],
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: `./img/${fileName(`[ext]`)}`,
+                    }
+                }]
+            },
+            {
+                test: /\.js$/, 
+                exclude: /node_modules/,
+                use: [
+                    "babel-loader"
+                ]
+                
+            },
+            // {
+            //     test: /\.(png|jpe?g|gif)$/i,
+            //     type: 'asset/resource',
+            //     use: [{
+            //         loader: 'file-loader',
+            //         options: {
+            //             name: `./img/${fileName(`[ext]`)}`,
+            //             limit: 10000,
+            //             esModule: false
+            //         }
+            //     }]
+            // }
+        ]
+    }
 };
